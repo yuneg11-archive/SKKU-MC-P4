a3ca23f6d83d1a3cecdd10a3bb8c42bffa4b1b0c
Eunggu Yun <yuneg11@gmail.com>
2019-12-03 02:52:23 +0900

    Add result evaluation

--- driver.cpp
+++ driver.cpp
@@ -1,28 +1,110 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <time.h>
 #include <cuda_runtime.h>
 
-#define N 1000000
-#define MAX_VAL 1000000
+#define N 500000000 // 1.86 GB
+#define MAX_VAL 1000000 // 3.81 MB
 
 extern void counting_sort(int arr[], int size, int max_val);
+void host_counting_sort(int arr[], int size, int max_val) {
+    int *histogram = new int[max_val]();
 
-int main() {
-    int array[N];
-
-    for(int i = 0; i < N; i++){
-        array[i] = rand() % MAX_VAL;
+    for (int i = 0; i < size; i++) {
+        histogram[arr[i]]++;
     }
 
-    counting_sort(array, N, MAX_VAL);
-
-    for(int i = 0; i < N - 1; i++){
-        if (array[i] > array[i + 1]) {
-            printf("Not sorted\n");
-            return -1;
+    for (int idx = 0, i = 0; i < max_val; i++) {
+        for (int j = 0; j < histogram[i]; j++) {
+            arr[idx++] = i;
         }
     }
-    printf("Sorted\n");
+
+    delete [] histogram;
+}
+
+bool is_sorted(int arr[], int size) {
+    for(int i = 0; i < size - 1; i++){
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool is_equal(int arr1[], int arr2[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr1[i] != arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+double get_elapse_time(struct timespec start, struct timespec end) {
+    return (end.tv_sec - start.tv_sec) + ((double) (end.tv_nsec - start.tv_nsec)) / 1000000000;
+}
+
+int main(int argc, char *argv[]) {
+    bool cuda = true;
+    bool verify = false;
+
+    if (argc == 2) {
+        if (strcmp(argv[1], "host") == 0) {
+            cuda = false;
+        } else if (strcmp(argv[1], "verify") == 0) {
+            verify = true;
+        }
+    }
+
+    int *arr_device = (cuda ? new int[N] : NULL);
+    int *arr_host = (cuda == verify ? new int[N] : NULL);
+    struct timespec time1, time2, time3;
+
+    if (cuda && !verify) {
+        printf("Device only\n");
+
+        for(int i = 0; i < N; i++){
+            arr_device[i] = rand() % MAX_VAL;
+        }
+
+        clock_gettime(CLOCK_REALTIME, &time1);
+        counting_sort(arr_device, N, MAX_VAL);
+        clock_gettime(CLOCK_REALTIME, &time2);
+
+        printf("Device sorting time: %.3lf secs\n", get_elapse_time(time1, time2));
+        printf("%s\n", (is_sorted(arr_device, N) ? "Sorted" : "Not sorted"));
+    } else if (!cuda && !verify) {
+        printf("Host only\n");
+
+        for(int i = 0; i < N; i++){
+            arr_host[i] = rand() % MAX_VAL;
+        }
+
+        clock_gettime(CLOCK_REALTIME, &time1);
+        host_counting_sort(arr_host, N, MAX_VAL);
+        clock_gettime(CLOCK_REALTIME, &time2);
+
+        printf("Host sorting time: %.3lf secs\n", get_elapse_time(time1, time2));
+        printf("%s\n", (is_sorted(arr_host, N) ? "Sorted" : "Not sorted"));
+    } else if (cuda && verify) {
+        printf("Device and Host\n");
+
+        for(int i = 0; i < N; i++){
+            arr_device[i] = arr_host[i] = rand() % MAX_VAL;
+        }
+
+        clock_gettime(CLOCK_REALTIME, &time1);
+        counting_sort(arr_device, N, MAX_VAL);
+        clock_gettime(CLOCK_REALTIME, &time2);
+        host_counting_sort(arr_host, N, MAX_VAL);
+        clock_gettime(CLOCK_REALTIME, &time3);
+
+        printf("Device sorting time: %.3lf secs\n", get_elapse_time(time1, time2));
+        printf("Host sorting time: %.3lf secs\n", get_elapse_time(time2, time3));
+        printf("%s\n", (is_equal(arr_device, arr_host, N) ? "Sorted (verified)" : "Not sorted"));
+    }
 
     return 0;
 }
